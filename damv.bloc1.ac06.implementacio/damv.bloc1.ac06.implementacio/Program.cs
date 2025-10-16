582496232c134faf15f9b70112320d2d0a23d2fa
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-10-16 12:08:44 +0200

    Double to floar. Refactoring

--- damv.bloc1.ac06.implementacio/damv.bloc1.ac06.implementacio/Program.cs
+++ damv.bloc1.ac06.implementacio/damv.bloc1.ac06.implementacio/Program.cs
@@ -2,18 +2,18 @@
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
         const string MsgInputWidth = "Enter the width of the rectangle: ";
         const string MsgInputHeight = "Enter the height of the rectangle: ";
         const string MsgResult = "The area of the rectangle is: ";
 
-        double width, height, area;
+        float width, height, area;
 
         Console.Write(MsgInputWidth);
-        width = Convert.ToDouble(Console.ReadLine());
+        width = (float)Convert.ToDouble(Console.ReadLine());
         Console.Write(MsgInputHeight);
-        height = Convert.ToDouble(Console.ReadLine());
+        height = (float)Convert.ToDouble(Console.ReadLine());
         area = width * height;
         Console.WriteLine(MsgResult + area);
     }
