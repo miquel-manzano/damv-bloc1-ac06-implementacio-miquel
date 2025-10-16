e21501b0006bb55c2fd3e6184f1d11d190ac436e
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-10-16 10:25:58 +0200

    Solution for Ex4 finished.

--- damv.bloc1.ac06.implementacio/damv.bloc1.ac06.implementacio/Program.cs
+++ damv.bloc1.ac06.implementacio/damv.bloc1.ac06.implementacio/Program.cs
@@ -2,6 +2,15 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        const double ExchangeRate = 1.12;
+        const string MsgInput = "Enter amount in euros: ";
+        const string MsgResult = "Amount in dollars: ";
+
+        double euros, dollars;
+
+        Console.Write(MsgInput);
+        euros = Convert.ToDouble(Console.ReadLine());
+        dollars = euros * ExchangeRate;
+        Console.WriteLine(MsgResult + dollars);
     }
 }
