12d81ffb3ad52a3ee06fb79d3bac8e699c1a547d
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-10-16 10:22:46 +0200

    Solution for Ex3 finished.

--- damv.bloc1.ac06.implementacio/damv.bloc1.ac06.implementacio/Program.cs
+++ damv.bloc1.ac06.implementacio/damv.bloc1.ac06.implementacio/Program.cs
@@ -1,7 +1,20 @@
-﻿public class Program
+﻿using Microsoft.VisualBasic;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        const string MsgInputWidth = "Enter the width of the rectangle: ";
+        const string MsgInputHeight = "Enter the height of the rectangle: ";
+        const string MsgResult = "The area of the rectangle is: ";
+
+        double width, height, area;
+
+        Console.Write(MsgInputWidth);
+        width = Convert.ToDouble(Console.ReadLine());
+        Console.Write(MsgInputHeight);
+        height = Convert.ToDouble(Console.ReadLine());
+        area = width * height;
+        Console.WriteLine(MsgResult + area);
     }
 }
