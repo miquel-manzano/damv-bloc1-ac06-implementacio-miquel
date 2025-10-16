98a96afa3ce1e3197db8b23f7b85c4333136d321
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-10-16 10:19:10 +0200

    Solution for Ex2 finished.

--- damv.bloc1.ac06.implementacio/damv.bloc1.ac06.implementacio/Program.cs
+++ damv.bloc1.ac06.implementacio/damv.bloc1.ac06.implementacio/Program.cs
@@ -2,6 +2,14 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        const string MsgInput = "Enter temperature in Celsius: ";
+        const string MsgResult = "Temperature in Kelvin: ";
+
+        double celsius, kelvin;
+
+        Console.Write(MsgInput);
+        celsius = Convert.ToDouble(Console.ReadLine());
+        kelvin = celsius + 273.15;
+        Console.WriteLine(MsgResult + kelvin);
     }
 }
