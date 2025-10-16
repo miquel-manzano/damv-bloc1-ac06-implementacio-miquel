c697b8397700487b4a9b2caf1a20cdf397e9f091
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-10-16 10:15:44 +0200

    Soution for ex1 finished

--- damv.bloc1.ac06.implementacio/damv.bloc1.ac06.implementacio/Program.cs
+++ damv.bloc1.ac06.implementacio/damv.bloc1.ac06.implementacio/Program.cs
@@ -2,6 +2,17 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        const string MsgInput1 = "Enter the first integer:";
+        const string MsgInput2 = "Enter the second integer";
+        const string MsgResult = "The Sum is: ";
+
+        int num1, num2, sum;
+
+        Console.WriteLine(MsgInput1);
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(MsgInput2);
+        num2 = Convert.ToInt32(Console.ReadLine());
+        sum = num1 + num2;
+        Console.WriteLine(MsgResult + sum);
     }
 }
