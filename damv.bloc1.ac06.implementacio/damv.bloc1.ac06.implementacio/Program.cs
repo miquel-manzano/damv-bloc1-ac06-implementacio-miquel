9322fd6e02640d2bdfed62865b2e9e35f41bf8d1
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-10-16 12:09:19 +0200

    Double to float. Refactoring

--- damv.bloc1.ac06.implementacio/damv.bloc1.ac06.implementacio/Program.cs
+++ damv.bloc1.ac06.implementacio/damv.bloc1.ac06.implementacio/Program.cs
@@ -1,20 +1,20 @@
 ﻿public class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
         const string MsgInput1 = "Enter the first number: ";
         const string MsgInput2 = "Enter the second number: ";
         const string MsgInput3 = "Enter the third number: ";
         const string MsgResult = "The average is: ";
 
-        double num1, num2, num3, average;
+        float num1, num2, num3, average;
 
         Console.Write(MsgInput1);
-        num1 = Convert.ToDouble(Console.ReadLine());
+        num1 = (float)Convert.ToDouble(Console.ReadLine());
         Console.Write(MsgInput2);
-        num2 = Convert.ToDouble(Console.ReadLine());
+        num2 = (float)Convert.ToDouble(Console.ReadLine());
         Console.Write(MsgInput3);
-        num3 = Convert.ToDouble(Console.ReadLine());
+        num3 = (float)Convert.ToDouble(Console.ReadLine());
         average = (num1 + num2 + num3) / 3;
         Console.Write(MsgResult + average);
     }
